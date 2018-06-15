395d332e46fea20413581b2e3786991206293a31
Byeong Gyu Choi <bisschoi9541@gmail.com>
2018-06-15 12:24:21 +0900

    Added icon for free book message, and added configurations regarding to it

--- src/__init__.py
+++ src/__init__.py
@@ -4,3 +4,7 @@ import sys
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 ENV = os.environ.get('PROJECT_ENV', 'development')
+
+BRANCH = 'master' if ENV == 'production' else 'develop'
+
+ASSETS_URL = 'https://raw.githubusercontent.com/depromeet/slackbot'
